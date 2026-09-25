68bcdd1222ff5414c6ed03e12f15def375adb15d
mkmalakzai <mkmalakzai@gmail.com>
2026-09-25 14:10:35 +0430

    Make my_stats a Bots.Business importable BJS command

--- commands/my_stats.js
+++ commands/my_stats.js
@@ -1,3 +1,12 @@
+/*CMD
+  command: my_stats
+  help:
+  need_reply: false
+  auto_retry_time:
+  folder: ACCOUNT
+  aliases:
+CMD*/
+
 // Bots.Business command: my_stats
 Bot.sendInlineKeyboard(
   [[{ title: "🏠 Main Menu", command: "main_menu" }]],
