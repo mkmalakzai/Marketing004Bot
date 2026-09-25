184307866f6e567c7c9f66ff1ff5d92c94ccd765
mkmalakzai <mkmalakzai@gmail.com>
2026-09-25 14:10:37 +0430

    Make support a Bots.Business importable BJS command

--- commands/support.js
+++ commands/support.js
@@ -1,3 +1,12 @@
+/*CMD
+  command: support
+  help:
+  need_reply: false
+  auto_retry_time:
+  folder: SUPPORT
+  aliases:
+CMD*/
+
 // Bots.Business command: support
 Bot.sendInlineKeyboard(
   [[{ title: "🏠 Main Menu", command: "main_menu" }]],
