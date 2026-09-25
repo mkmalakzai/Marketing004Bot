ae3ee0afaf144b18a69b7591077c8223ae91d11c
mkmalakzai <mkmalakzai@gmail.com>
2026-09-25 14:24:15 +0430

    Connect my_orders to TPL-004 app flow

--- commands/my_orders.js
+++ commands/my_orders.js
@@ -2,13 +2,6 @@
   command: my_orders
   help:
   need_reply: false
-  auto_retry_time:
   folder: ORDERS
-  aliases:
 CMD*/
-
-// Bots.Business command: my_orders
-Bot.sendInlineKeyboard(
-  [[{ title: "🏠 Main Menu", command: "main_menu" }]],
-  "📦 MY ORDERS\n\nOrdering is being prepared. No orders are active yet."
-);
+Bot.runCommand("app my_orders");
