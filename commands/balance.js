2f2c9a1688ccb461cfb361a8dd48c52e0e1545d3
mkmalakzai <mkmalakzai@gmail.com>
2026-09-25 14:24:17 +0430

    Connect balance to TPL-004 app flow

--- commands/balance.js
+++ commands/balance.js
@@ -2,13 +2,6 @@
   command: balance
   help:
   need_reply: false
-  auto_retry_time:
   folder: WALLET
-  aliases:
 CMD*/
-
-// Bots.Business command: balance
-Bot.sendInlineKeyboard(
-  [[{ title: "🏠 Main Menu", command: "main_menu" }]],
-  "💰 BALANCE\n\nDeposits and balance payments are being prepared. Do not send a payment yet."
-);
+Bot.runCommand("app balance");
