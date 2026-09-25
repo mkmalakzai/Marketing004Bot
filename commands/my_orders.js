0e4812f7b80a3e0e8e85b9b85ba1f6582927a900
mkmalakzai <mkmalakzai@gmail.com>
2026-09-25 14:10:29 +0430

    Make my_orders a Bots.Business importable BJS command

--- commands/my_orders.js
+++ commands/my_orders.js
@@ -1,3 +1,12 @@
+/*CMD
+  command: my_orders
+  help:
+  need_reply: false
+  auto_retry_time:
+  folder: ORDERS
+  aliases:
+CMD*/
+
 // Bots.Business command: my_orders
 Bot.sendInlineKeyboard(
   [[{ title: "🏠 Main Menu", command: "main_menu" }]],
