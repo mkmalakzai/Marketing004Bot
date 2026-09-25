16295e96b598b5c94b718db50f83fb5def1e2b0d
mkmalakzai <mkmalakzai@gmail.com>
2026-09-25 14:10:34 +0430

    Make offers a Bots.Business importable BJS command

--- commands/offers.js
+++ commands/offers.js
@@ -1,3 +1,12 @@
+/*CMD
+  command: offers
+  help:
+  need_reply: false
+  auto_retry_time:
+  folder: OFFERS
+  aliases:
+CMD*/
+
 // Bots.Business command: offers
 Bot.sendInlineKeyboard(
   [[{ title: "🏠 Main Menu", command: "main_menu" }]],
