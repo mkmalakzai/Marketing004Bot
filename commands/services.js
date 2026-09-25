ae8b68f43314e32c5d058c8ba51d790a707c91e6
mkmalakzai <mkmalakzai@gmail.com>
2026-09-25 14:24:14 +0430

    Connect services to TPL-004 app flow

--- commands/services.js
+++ commands/services.js
@@ -2,13 +2,6 @@
   command: services
   help:
   need_reply: false
-  auto_retry_time:
   folder: CATALOG
-  aliases:
 CMD*/
-
-// Bots.Business command: services
-Bot.sendInlineKeyboard(
-  [[{ title: "🏠 Main Menu", command: "main_menu" }]],
-  "🚀 SERVICES\n\nCategories and packages are being prepared."
-);
+Bot.runCommand("app services");
