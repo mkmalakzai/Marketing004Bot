93a44c89e9690f3c85f7edb905ce0e1ebc9ae665
mkmalakzai <mkmalakzai@gmail.com>
2026-09-25 14:24:10 +0430

    Connect main_menu to TPL-004 app flow

--- commands/main_menu.js
+++ commands/main_menu.js
@@ -2,16 +2,6 @@
   command: main_menu
   help:
   need_reply: false
-  auto_retry_time:
   folder: CORE
-  aliases:
 CMD*/
-
-// Bots.Business command: main_menu
-var buttons = [
-  [{ title: "🚀 Services", command: "services" }, { title: "📦 My Orders", command: "my_orders" }],
-  [{ title: "💰 Balance", command: "balance" }, { title: "🎁 Offers", command: "offers" }],
-  [{ title: "📊 My Stats", command: "my_stats" }, { title: "🎫 Support", command: "support" }],
-  [{ title: "👤 My Account", command: "my_account" }]
-];
-Bot.sendInlineKeyboard(buttons, "🏠 MARKETING & PROMOTION\n\nChoose an option below.");
+Bot.runCommand("app home");
