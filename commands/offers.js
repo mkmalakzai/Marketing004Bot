dc3e44d47ab9bb0837850094fa0843bcd93e269b
mkmalakzai <mkmalakzai@gmail.com>
2026-09-25 14:24:18 +0430

    Connect offers to TPL-004 app flow

--- commands/offers.js
+++ commands/offers.js
@@ -2,13 +2,6 @@
   command: offers
   help:
   need_reply: false
-  auto_retry_time:
   folder: OFFERS
-  aliases:
 CMD*/
-
-// Bots.Business command: offers
-Bot.sendInlineKeyboard(
-  [[{ title: "🏠 Main Menu", command: "main_menu" }]],
-  "🎁 OFFERS\n\nOffers are being prepared."
-);
+Bot.runCommand("app offers");
