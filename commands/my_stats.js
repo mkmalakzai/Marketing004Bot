ab479b2665e6819940fdd2440b4e9515039ae7f2
mkmalakzai <mkmalakzai@gmail.com>
2026-09-25 14:24:20 +0430

    Connect my_stats to TPL-004 app flow

--- commands/my_stats.js
+++ commands/my_stats.js
@@ -2,13 +2,6 @@
   command: my_stats
   help:
   need_reply: false
-  auto_retry_time:
   folder: ACCOUNT
-  aliases:
 CMD*/
-
-// Bots.Business command: my_stats
-Bot.sendInlineKeyboard(
-  [[{ title: "🏠 Main Menu", command: "main_menu" }]],
-  "📊 MY STATS\n\nOrder statistics will appear after ordering is enabled."
-);
+Bot.runCommand("app stats");
