6789301622e5a58a5dd74c5b2599e6e994f4ea32
mkmalakzai <mkmalakzai@gmail.com>
2026-09-25 14:24:22 +0430

    Connect support to TPL-004 app flow

--- commands/support.js
+++ commands/support.js
@@ -2,13 +2,6 @@
   command: support
   help:
   need_reply: false
-  auto_retry_time:
   folder: SUPPORT
-  aliases:
 CMD*/
-
-// Bots.Business command: support
-Bot.sendInlineKeyboard(
-  [[{ title: "🏠 Main Menu", command: "main_menu" }]],
-  "🎫 SUPPORT\n\nSupport tickets are being prepared."
-);
+Bot.runCommand("app support");
