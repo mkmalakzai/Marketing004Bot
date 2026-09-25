0ad8d05a933c721fddcb23f1a30adc92b6a37a45
mkmalakzai <mkmalakzai@gmail.com>
2026-09-25 14:10:24 +0430

    Make main_menu a Bots.Business importable BJS command

--- commands/main_menu.js
+++ commands/main_menu.js
@@ -1,3 +1,12 @@
+/*CMD
+  command: main_menu
+  help:
+  need_reply: false
+  auto_retry_time:
+  folder: CORE
+  aliases:
+CMD*/
+
 // Bots.Business command: main_menu
 var buttons = [
   [{ title: "🚀 Services", command: "services" }, { title: "📦 My Orders", command: "my_orders" }],
