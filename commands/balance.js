12c1312fefc44e394d5c5c032151241c26fb182d
mkmalakzai <mkmalakzai@gmail.com>
2026-09-25 14:10:31 +0430

    Make balance a Bots.Business importable BJS command

--- commands/balance.js
+++ commands/balance.js
@@ -1,3 +1,12 @@
+/*CMD
+  command: balance
+  help:
+  need_reply: false
+  auto_retry_time:
+  folder: WALLET
+  aliases:
+CMD*/
+
 // Bots.Business command: balance
 Bot.sendInlineKeyboard(
   [[{ title: "🏠 Main Menu", command: "main_menu" }]],
