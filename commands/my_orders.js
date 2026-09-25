752fd119b8a9932605a9db9049f387e4bf04cb7e
mkmalakzai <mkmalakzai@gmail.com>
2026-09-25 13:55:53 +0430

    Add TPL-004 my_orders route

---
+++ commands/my_orders.js
@@ -0,0 +1,5 @@
+// Bots.Business command: my_orders
+Bot.sendInlineKeyboard(
+  [[{ title: "🏠 Main Menu", command: "main_menu" }]],
+  "📦 MY ORDERS\n\nOrdering is being prepared. No orders are active yet."
+);
